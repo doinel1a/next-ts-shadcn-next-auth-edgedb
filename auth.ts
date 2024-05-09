997cad63c0841dbe4a1193781643d5b3dddee0a9
doinel1a <doinel1atanasiu@gmail.com>
2024-05-09 22:33:00 +0200

    chore: init edgedb & configure w/ next-auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
+import { EdgeDBAdapter } from '@auth/edgedb-adapter';
+import { createClient } from 'edgedb';
 import NextAuth from 'next-auth';
 
+const client = createClient({ dsn: process.env.AUTH_EDGEDB_DSN });
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: EdgeDBAdapter(client),
   providers: []
 });
 
