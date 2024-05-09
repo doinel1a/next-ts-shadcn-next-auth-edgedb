dc477937dc9992e64c37ae19efd517446039a665
doinel1a <doinel1atanasiu@gmail.com>
2024-05-09 22:33:27 +0200

    chore(next-auth): add github as oauth provider

--- auth.ts
+++ auth.ts
@@ -1,11 +1,12 @@
 import { EdgeDBAdapter } from '@auth/edgedb-adapter';
 import { createClient } from 'edgedb';
 import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
 
 const client = createClient({ dsn: process.env.AUTH_EDGEDB_DSN });
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: EdgeDBAdapter(client),
-  providers: []
+  providers: [Github]
 });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
+import { auth } from 'auth';
+
 import Counter from '@/components/counter';
 import GithubCorner from '@/components/github-corner';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <main className='flex h-full w-full flex-col items-center justify-center'>
       <GithubCorner
         title='Get started on GitHub'
         url='https://github.com/doinel1a/next-ts-shadcn-ui'
       />
+
+      {session?.user?.name && (
+        <h1 className='mb-2.5 text-3xl font-bold'>Welcome {session.user.name}</h1>
+      )}
+
       <Counter />
     </main>
   );
