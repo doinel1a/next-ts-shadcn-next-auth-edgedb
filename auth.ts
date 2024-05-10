2aa9d388bcf3c328eac695cec8ebc4b2d4a5750f
doinel1a <doinel1atanasiu@gmail.com>
2024-05-10 17:59:16 +0200

    chore(auth.ts): export client

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { createClient } from 'edgedb';
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github';
 
-const client = createClient();
+export const client = createClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: EdgeDBAdapter(client),
